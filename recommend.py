ee5743a409722abc7ee93d0874d341609f06fc92
Abhinav Mittra <abhinavmittra@yahoo.com>
2018-05-26 23:15:29 +0530

    Add files

---
+++ recommend.py
@@ -0,0 +1,4 @@
+import pandas as pd
+3feature_file = 'https://static.turi.com/datasets/millionsong/10000.txt'  #user_id,song_id,listening time
+songsmetadata_file = 'https://static.turi.com/datasets/millionsong/song_data.csv' #song_id,title,released_by,artist_name
+
